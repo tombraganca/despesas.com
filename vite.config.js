cd71a89438c6ab82315f99b69eeb7f83c083e5da
Tom Santos <gleydiston35@gmail.com>
2024-03-16 16:18:56 -0300

    test: Adicionado Covarege para testes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = require("vitest/config");
 exports.default = (0, config_1.defineConfig)({
-    test: {}
+    test: {
+        coverage: {
+            provider: 'istanbul', // or 'v8',
+            exclude: [
+                './src/config',
+                './src/server.ts',
+                '**/routes.*',
+                '**/index.*'
+            ]
+        },
+    }
 });
 //# sourceMappingURL=vite.config.js.map
